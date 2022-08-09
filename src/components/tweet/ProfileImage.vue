4deeadc6613700c69c4d26a993908d64ad6ffb6f
Macia Estela <maciaestela97@gmail.com>
2022-08-09 20:53:25 +0200

    lab done

---
+++ src/components/tweet/ProfileImage.vue
@@ -0,0 +1,19 @@
+<template>
+    <img
+      :src="image"
+      className="profile"
+      alt="profile"
+    />
+</template>
+
+<script>
+export default {
+    props: {
+        image: String
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/tweet/User.vue
@@ -0,0 +1,16 @@
+<template>
+    <span className="user">
+        <span className="name">{{ username }}</span>
+        <span className="handle">@{{ handle }}</span>
+    </span>
+</template>
+
+<script>
+export default {
+    props:['username', 'handle']
+}
+</script>
+
+<style>
+
+</style>
