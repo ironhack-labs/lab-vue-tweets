62d8833bff90a90daf9ab5ef6a494b44b281b3ac
Anton Zyrianov <a.zyrianov.z@gmail.com>
2022-08-11 12:12:23 +0200

    lab done

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,38 +1,44 @@
 <template>
   <div className="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      className="profile"
-      alt="profile"
-    />
+
+    <ProfileImage :user="user" />
 
     <div className="body">
       <div className="top">
-        <span className="user">
-          <span className="name">Ironhack</span>
-          <span className="handle">@ironhack</span>
-        </span>
-
-        <span className="timestamp">Nov 30, 2020</span>
+        <User :user="user"/>
+        <Timestamp />
       </div>
+      <Message :message="message"/>
 
-      <p className="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
-      </p>
-
-      <div className="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
+      <Actions />
     </div>
 
     <i class="fas fa-ellipsis-h"></i>
   </div>
 </template>
+
+<script>
+
+import ProfileImage from "../components/ProfileImage.vue"
+import Message from "../components/Message.vue"
+import Timestamp from "../components/Timestamp.vue"
+import Actions from "../components/Actions.vue"
+import User from "../components/User.vue"
+
+export default({
+  name: "Tweet",
+  components: { ProfileImage, Message, Timestamp, Actions, User },
+  data() {
+    return{
+    }
+  },
+  props: {
+    user: Object,
+    message: String,
+  },
+})
+</script>
+
 
 <style scoped>
 a {
