87a3868838f6bb21fc0fe818815f15330f9fe70f
Mario <alloparramario@gmail.com>
2022-08-11 16:04:51 +0200

    lab done

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,16 @@
+<template>
+  <img src="user.image" className="profile" alt="profile"/>
+</template>
+
+<script>
+export default {
+   name: "ProfileImage",
+   props: {
+      user: Object,
+   }
+}
+</script>
+
+<style>
+
+</style>
