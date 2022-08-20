711a879040fa742530fd4e6fb559bc9b2816ec36
andayanda <carmelaespinola@gmail.com>
2022-08-20 14:13:06 +0200

    componente ProfileImage ok

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,14 @@
+<template>
+    <img
+      :src="image"
+      className="profile"
+      alt="profile"
+    />    
+</template>
+<script>
+export default {
+    props:{
+        image: String,
+    }
+}
+</script>
