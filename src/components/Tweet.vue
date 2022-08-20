dc81e14a1046ae2311565643530ced62344f3ac9
Gianluca <gianluca@inovum-solutions.com>
2022-08-20 14:40:19 +0200

    done iterations 1 and 2

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,41 +1,47 @@
 <template>
-  <div className="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      className="profile"
-      alt="profile"
-    />
+	<div className="tweet">
+		<img :src="tweet.user.image" className="profile" alt="profile" />
 
-    <div className="body">
-      <div className="top">
-        <span className="user">
-          <span className="name">Ironhack</span>
-          <span className="handle">@ironhack</span>
-        </span>
+		<div className="body">
+			<div className="top">
+				<span className="user">
+					<span className="name">{{ tweet.user.name }}</span>
+					<span className="handle">@{{ tweet.user.handle }}</span>
+				</span>
 
-        <span className="timestamp">Nov 30, 2020</span>
-      </div>
+				<span className="timestamp">{{ tweet.timestamp }}</span>
+			</div>
 
-      <p className="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
-      </p>
+			<p className="message">
+				{{ tweet.message }}
+			</p>
 
-      <div className="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
-    </div>
+			<div className="actions">
+				<!-- Font Awesome icons -->
+				<i class="far fa-comment"></i>
+				<i class="fas fa-retweet"></i>
+				<i class="far fa-heart"></i>
+				<i class="fas fa-share"></i>
+			</div>
+		</div>
 
-    <i class="fas fa-ellipsis-h"></i>
-  </div>
+		<i class="fas fa-ellipsis-h"></i>
+	</div>
 </template>
-
+<script>
+export default {
+	name: 'Tweet',
+	props: {
+		tweet: {
+			user: Object,
+			timestamp: String,
+			message: String
+		},
+	}
+}
+</script>
 <style scoped>
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
