c74678b8cc0e2c1591d43e9b0889c5d33a1a46a8
Álvaro Noguer <alvaronoguer@gmail.com>
2022-08-20 14:16:11 +0200

    Lab done

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,43 +1,31 @@
 <template>
   <div className="tweet">
-    <img
-      :src=tweet.user.image
-      className="profile"
-      alt="profile"
-    />
-
+    <ProfileImage :image="tweet.user.image" />
     <div className="body">
       <div className="top">
-        <span className="user">
-          <span className="name">{{tweet.user.name}}</span>
-          <span className="handle">{{tweet.user.handle}}</span>
-        </span>
-
-        <span className="timestamp">{{tweet.handle}}</span>
+        <User :name="tweet.user.name" :handle="tweet.user.handle" />
+        <Timestamp :timestamp="tweet.timestamp" />
       </div>
-
-      <p className="message">{{tweet.message}}</p>
-
-      <div className="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
+      <Message :message="tweet.message" />     
+      <Actions /> 
     </div>
-
-    <i class="fas fa-ellipsis-h"></i>
   </div>
 </template>
 
 <script>
-export default {
-      props: {
-        tweet: Object
-    },
-}
+import ProfileImage from './ProfileImage.vue';
+import User from './User.vue';
+import Timestamp from './Timestamp.vue';
+import Message from './Message.vue';
+import Actions from './Actions.vue';
 
+export default {
+    name: "Tweet",
+    props: {
+        tweet: Object,
+    },
+    components: { ProfileImage, User, Timestamp, Message, Actions }
+}
 </script>
 
 
