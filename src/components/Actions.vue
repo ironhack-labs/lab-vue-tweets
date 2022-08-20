5ec5e34818414900b6cbb9a80f3755ca34d5a631
Txell <meritxell@gold.barcelona>
2022-08-20 14:50:52 +0200

    iteration 8

--- src/components/Actions.vue
+++ src/components/Actions.vue
@@ -1,5 +1,10 @@
 <template>
-  
+  <div className="actions">
+    <i class="far fa-comment"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="far fa-heart"></i>
+    <i class="fas fa-share"></i>
+  </div>
 </template>
 
 <script>
--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -9,14 +9,7 @@
       </div>
 
       <Message :message= "message" />
-
-      <div className="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
+      <Actions />
     </div>
 
     <i class="fas fa-ellipsis-h"></i>
@@ -27,6 +20,7 @@
   import User from './User.vue';
   import Timestamp from './Timestamp.vue';
   import Message from './Message.vue';
+  import Actions from './Actions.vue';
 
   export default {
     props: {
@@ -43,6 +37,7 @@
       User,
       Timestamp,
       Message,
+      Actions,
     },
   }
 </script>
